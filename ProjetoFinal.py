b79e0fc0b646a16116d2c990b45172eceba8930e
Andre-Weber <andre.deweber@gmail.com>
2018-05-15 16:34:01 -0300

    Drag and drop

--- ProjetoFinal.py
+++ ProjetoFinal.py
@@ -10,31 +10,66 @@ import sys
 from pygame.locals import *
 import random
 
+
+
+
 #sys.path.append('./data')
 
-janela = pygame.display.set_mode((1020, 333), 0, 32)
+janela = pygame.display.set_mode((1200, 333), 0, 32)
 pygame.display.set_caption('Batalha Naval')
 imagem = pygame.image.load("batalha.jpg").convert()
 imagem2 = pygame.image.load("batalha.jpg").convert()
 water = pygame.image.load("agua.png").convert()
 img_botao = pygame.image.load("botao.png").convert()
-coord = [551,28]
+bola17 = pygame.image.load("bola17.png").convert()
+preto = pygame.image.load("preto.png").convert()
+coord = [731,28]
 quadrado = [43,39]
-unicolugar = False
-atual = [0,0]
+tam_navio = [[43,39],[86,39],[129,39],[172,39]]
+coord_navio = [[18,8],[18,70],[18,176],[118,8]]
+navio1 = pygame.image.load("navio1.png").convert()
+navio2 = pygame.image.load("navio2.png").convert()
+navio3 = pygame.image.load("navio3.png").convert()
+navio4 = pygame.image.load("navio4.png").convert()
+navio1 = pygame.transform.rotate(navio1, 270)
+navio2 = pygame.transform.rotate(navio2, 270)
+navio3 = pygame.transform.rotate(navio3, 270)
+navio4 = pygame.transform.rotate(navio4, 270)
+
+listanavio = [navio1,navio2,navio3,navio4]
+
 def Game():
-    unicolugar = False  
-    atual = [0,0]
+    mousepressed_up = [False,-1]
     i = 1
     while True:
-        janela.blit(imagem, (0, 0))
-        janela.blit(imagem2,(510,0))
+        janela.blit(imagem, (180, 0))
+        janela.blit(imagem2,(690,0))
+        janela.blit(preto, (0,0))
+        
         xx,yy = pygame.mouse.get_pos()
+        print(xx,yy)
+        if mousepressed_up[0]:
+            coord_navio[mousepressed_up[1]]=[xx,yy]
+            
+                    
+                
+        for event in pygame.event.get():
+            for i in range(len(coord_navio)):
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if xx > coord_navio[i][0] and xx < coord_navio[i][0] + 39 and yy > coord_navio[i][1] and yy < coord_navio[i][1] + tam_navio[i][0]:
+                        mousepressed_up=[True,i]
+                if event.type == pygame.MOUSEBUTTONUP:
+                    mousepressed_up=[False,i]
+
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+        for i in range(len(coord_navio)):
+             janela.blit(listanavio[i], (coord_navio[i][0],coord_navio[i][1]))
+                    
         i = 1
         x = 1
-        if unicolugar:
-            janela.blit(water,(atual[0],atual[1]))
-        while i < 11 and x < 9:
+        
+        while i < 11 and x < 8:
             if xx > coord[0]+(i-1)*quadrado[0] and yy > coord[1] + (x-1)*quadrado[1]  and xx < coord[0]+i*quadrado[0] and yy < coord[1] + x*quadrado[1]:
                 janela.blit(img_botao,(coord[0]+(i-1)*quadrado[0],coord[1]+(x-1)*quadrado[1]))
             i += 1
@@ -45,17 +80,16 @@ def Game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-            if event.type == pygame.MOUSEBUTTONUP:
-                pox,poy = pygame.mouse.get_pos()
-                if pox > coord[0] and poy > coord[1] and pox < coord[0]+quadrado[0] and poy < coord[1] + quadrado[1]:
-                    unicolugar=True
-                    atual=[coord[0],coord[1]]                        
-                else:
-                    pass#unicolugar=False                   				    
-		
+            
+                            				    
 					
         pygame.display.update()
 Game()
 
 
+                
+                        
+            
+            
+            
             
